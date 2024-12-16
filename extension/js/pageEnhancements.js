54f19db9199dcaf8619de2267421aee9cd2de257
Chucem <chucemmimanistanu@gmail.com>
2024-12-16 15:14:16 -0500

    fix sticky bugs collide with absolute of the element

--- extension/js/pageEnhancements.js
+++ extension/js/pageEnhancements.js
@@ -1,14 +1,32 @@
+const TOP_OF_FIRST_PAGE_BUTTONS = document.querySelector('.has-profile .post-buttons')?.getBoundingClientRect().top;
+
 function enchancePage() {
   const url = new URL(window.location.href);
   const params = new URLSearchParams(url.search);
 
 
   if (document.getElementById("iveltHelperSettings").getAttribute("data-sticky-post-buttons") === "true") {
-    document.querySelectorAll('.has-profile .post-buttons').forEach(btn => {
-        btn.style.position = "sticky";
-        btn.style.top = "5px";
-        btn.style.zIndex = "1";
+
+    window.addEventListener('scroll', function() {
+      const postButtons = document.querySelectorAll('.has-profile .post-buttons');
+
+      // Get the distance from the top of the document
+      const scrollY = window.scrollY || window.pageYOffset;
+      // When you scroll past a certain point, switch to sticky
+      if (scrollY > TOP_OF_FIRST_PAGE_BUTTONS) {
+        postButtons.forEach(btn => {
+              btn.classList.add('sticky-post');
+        });
+      } else {
+        postButtons.forEach(btn => {
+          btn.classList.remove('sticky-post');
+        });
+      }
     });
+
+    // document.querySelectorAll('.has-profile .post-buttons').forEach(btn => {
+    //       btn.classList.add('sticky-post');
+    // });
   }
   // Only run on Topic page
   if (url.pathname === "/forum/viewtopic.php"){
